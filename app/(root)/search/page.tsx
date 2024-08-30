2ded0ec4ba8cb12397f97f7ecae33db644539d75
Gaoridang <oingc@naver.com>
2024-08-30 17:18:17 +0900

    Refactor: can send message with enter key

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React, { useState } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import { search } from "./search";
 import { MessageType } from "@/types";
 import Message from "./components/Message";
@@ -10,8 +10,16 @@ import Message from "./components/Message";
 const SearchPage = () => {
   const [messages, setMessages] = useState<MessageType[]>([]);
   const [query, setQuery] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSendMessage = async () => {
+  const handleSendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!query) {
+      setError("메시지를 입력하세요.");
+      return;
+    }
+
     setQuery("");
 
     setMessages((prev) => [
@@ -27,16 +35,24 @@ const SearchPage = () => {
     ]);
   };
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    setError("");
+  };
+
   return (
     <div>
-      <div className="flex gap-2">
-        <Input
-          placeholder="메시지를 입력하세요."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <Button onClick={handleSendMessage}>전송</Button>
-      </div>
+      <form className="grid" onSubmit={handleSendMessage}>
+        <div className="flex gap-2">
+          <Input
+            placeholder="어제 뭐 먹었더라?"
+            value={query}
+            onChange={handleChange}
+          />
+          <Button type="submit">전송</Button>
+        </div>
+        {error && <div className="text-red-500 text-sm mt-2">{error}</div>}
+      </form>
 
       <div className="border rounded-lg p-4 mt-4 space-y-2">
         {messages.map((message) => (
